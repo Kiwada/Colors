e64088c80f62f46cf000c7c9d0263a8aa4a26f2b
Kaio Fontenele <82303943+Kiwada@users.noreply.github.com>
2023-01-23 12:34:36 -0300

    Add files via upload

---
+++ corespython.py
@@ -0,0 +1,10 @@
+nome = 'Kaio'
+cor = {'limpa':'\033[m',
+         'azul':'\033[34m',
+         'amarelo':'\033[33m',
+         'vermelho':'\033[31m'}
+print('Ola! Muito prazer em te conhcer , {}{}{}'.format(cor['vermelho'],nome,cor['limpa']))
+
+         
+         
+                               
